0e8caaa84400f4a0a42610e897b4f78d91c9945b
Jay Parikh <jayparikh7@users.noreply.github.com>
2017-04-18 00:02:16 -0500

    Hmm, success, I guess

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -15,17 +15,30 @@ class MeteorologistController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + @street_address.gsub(" ", "+")
+    parsed_data = JSON.parse(open(url).read)
+    latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+    url2 = "https://api.darksky.net/forecast/fa207a48646e39d34ef26f7fa9e3d869/" + latitude.to_s + "," + longitude.to_s
+    parsed_data = JSON.parse(open(url2).read)
+
+    temp = parsed_data["currently"]["temperature"]
+    sum = parsed_data["currently"]["summary"]
+    sum_sixty_min = parsed_data["minutely"]["summary"]
+    sum_hours = parsed_data["hourly"]["summary"]
+    sum_days = parsed_data["daily"]["summary"]
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = temp
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = sum
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = sum_sixty_min
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = sum_hours
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = sum_days
 
     render("meteorologist/street_to_weather.html.erb")
   end
